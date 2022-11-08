c04996a2cd4e390d6f1dccdaef914aa73387de1e
Lisa Courtade <lisa.courtade@hotmail.fr>
2022-11-08 16:30:59 +0200

    added reset page for new search and added error text

--- app.js
+++ app.js
@@ -4,26 +4,36 @@ const input = document.querySelector('#title-searched');
 
 
 const getSingleShow = async function (searchedText) {
-    const result = await axios.get(`https://api.tvmaze.com/singlesearch/shows?q=${searchedText}`);
-    const show = result.data;
-    const showName = show.name;
-    const newH2 = document.createElement('h2');
-    newH2.append(showName);
-    container.appendChild(newH2);
-    const showGenres = show.genres;
-    const newP = document.createElement('p');
-    newP.append(showGenres.join(", "));
-    container.appendChild(newP);
-    if (show.image) {
-        const newImg = document.createElement('img');
-        newImg.src = show.image.medium;
-        container.appendChild(newImg);
+    try {
+        const result = await axios.get(`https://api.tvmaze.com/singlesearch/shows?q=${searchedText}`);
+        const show = result.data;
+        const showName = show.name;
+        const newH2 = document.createElement('h2');
+        newH2.append(showName);
+        container.appendChild(newH2);
+        const showGenres = show.genres;
+        const newP = document.createElement('p');
+        newP.append(showGenres.join(", "));
+        container.appendChild(newP);
+        if (show.image) {
+            const newImg = document.createElement('img');
+            newImg.src = show.image.medium;
+            container.appendChild(newImg);
+        }
+    } catch {
+        const newP = document.createElement('p');
+        newP.append(`Sorry, no Tv Shows found for ${searchedText}`);
+        container.appendChild(newP) 
     }
+    
 
 }
 
-form.addEventListener('submit', async (e) => {
+form.addEventListener('submit', async e => {
     e.preventDefault();
+    if (container.innerHTML) {
+        container.innerHTML = '';
+    }
     const searchedTitle = input.value;
     await getSingleShow(searchedTitle);
 })
