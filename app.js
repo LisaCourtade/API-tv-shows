de63ac06fc57a086db1b44d85cb3288b814cb52c
Lisa Courtade <lisa.courtade@hotmail.fr>
2022-11-15 14:19:31 +0200

    retrieved network in case no channel and retrieved dates of show from API. Added styles to the injected elements

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const getSingleShow = async function (searchedText) {
         const result = await axios.get(`https://api.tvmaze.com/singlesearch/shows?q=${searchedText}`);
         const show = result.data;
         // name of the show 
-        const showName = show.name;
+        const showName = show.name.toUpperCase();
         const newName = document.createElement('h2');
         newName.append(showName);
         sumUpBox.appendChild(newName);
@@ -42,21 +42,44 @@ const getInfoShow = async function (searchedText) {
         const result = await axios.get(`https://api.tvmaze.com/singlesearch/shows?q=${searchedText}`);
         const show = result.data;
         console.log(result.data);
-        // country of origin
+        // channel or network
         if (show.network) {
+            // country of origin
             const showCountry = show.network.country.name;
-            const newCountry = document.createElement('h3');
-            newCountry.append(showCountry);
+            const showNetwork = show.network.name;
+            const newCountry = document.createElement('h4');
+            newCountry.append(`${showCountry} - Network: ${showNetwork}`);
             sumUpBox.appendChild(newCountry);
         }
-        // To Add:
-        // .network.name = name of channel or network
-        // .network.premiered = date
-        // .status = "Ended" or "Running"
-        // ._links.ended = date
+        if (show.webChannel) {
+            const showNetwork = show.webChannel.name;
+            const newNetwork = document.createElement('h4');
+            newNetwork.append(`Network: ${showNetwork}`);
+            sumUpBox.appendChild(newNetwork);
+        }
+        // Dates
+        const newDates = document.createElement('h4');
+        // premiere
+        const showPremiere = show.premiered;
+        const newPremiere = document.createElement('span');
+        newPremiere.append(`First episode date: ${showPremiere} `);
+        newDates.appendChild(newPremiere);
+        // end
+        if (show.ended) {
+            const showEnd = show.ended;
+            const newEnd = document.createElement('span');
+            newEnd.append(`- Last episode date: ${showEnd}`);
+            newDates.appendChild(newEnd);
+        } else {
+            const newEnd = document.createElement('span');
+            newEnd.append(`- Show still running`);
+            newDates.appendChild(newEnd);
+        }
+        sumUpBox.appendChild(newDates);
         // summary
         const showSummary = show.summary;
         const newSumup = document.createElement('p');
+        newSumup.classList.add('summary');
         newSumup.innerHTML = showSummary;
         sumUpBox.appendChild(newSumup);
         // rating
@@ -67,13 +90,11 @@ const getInfoShow = async function (searchedText) {
             sumUpBox.appendChild(newRating);
         }
     } catch (e) {
-        const newP = document.createElement('p');
-        newP.append(`No information found for ${searchedText}`);
-        sumUpBox.appendChild(newP);
+        const newErr = document.createElement('p');
+        newErr.append(`No information found for ${searchedText}`);
+        sumUpBox.appendChild(newErr);
         console.log(e); 
     }
-    
-
 }
 
 form.addEventListener('submit', async e => {
