72492a80342d3bce047399798c38ed90ee79360e
Lisa Courtade <lisa.courtade@hotmail.fr>
2022-11-15 13:08:11 +0200

    removed rating from second API and retreived rating and country from main API. Refactored code into 2 async functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('#search-form');
 const imgBox = document.querySelector('#img-container');
-const SumupBox = document.querySelector('#summary-container');
+const sumUpBox = document.querySelector('#summary-container');
 const input = document.querySelector('#title-searched');
 
 
@@ -8,28 +8,68 @@ const getSingleShow = async function (searchedText) {
     try {
         const result = await axios.get(`https://api.tvmaze.com/singlesearch/shows?q=${searchedText}`);
         const show = result.data;
+        // name of the show 
         const showName = show.name;
-        const newH2 = document.createElement('h2');
-        newH2.append(showName);
-        imgBox.appendChild(newH2);
+        const newName = document.createElement('h2');
+        newName.append(showName);
+        sumUpBox.appendChild(newName);
+        // genres 
         const showGenres = show.genres;
-        const newGenres = document.createElement('p');
+        const newGenres = document.createElement('h3');
         newGenres.append(showGenres.join(", "));
-        imgBox.appendChild(newGenres);
-        const showSummary = show.summary;
-        const newSumup = document.createElement('p');
-        newSumup.innerHTML = showSummary;
-        SumupBox.appendChild(newSumup);
-        getImdb(showName);
+        sumUpBox.appendChild(newGenres);
+        // image
         if (show.image) {
             const newImg = document.createElement('img');
             newImg.src = show.image.medium;
             imgBox.appendChild(newImg);
         }
+        getInfoShow(searchedText);
+    } catch (e) {
+        const newErr = document.createElement('p');
+        newErr.append(`Sorry, no Tv Shows found for: ${searchedText}.`);
+        imgBox.appendChild(newErr);
+        console.log(e); 
+    }
+    
+
+}
+
+
+
+const getInfoShow = async function (searchedText) {
+    try {
+        const result = await axios.get(`https://api.tvmaze.com/singlesearch/shows?q=${searchedText}`);
+        const show = result.data;
+        console.log(result.data);
+        // country of origin
+        if (show.network) {
+            const showCountry = show.network.country.name;
+            const newCountry = document.createElement('h3');
+            newCountry.append(showCountry);
+            sumUpBox.appendChild(newCountry);
+        }
+        // To Add:
+        // .network.name = name of channel or network
+        // .network.premiered = date
+        // .status = "Ended" or "Running"
+        // ._links.ended = date
+        // summary
+        const showSummary = show.summary;
+        const newSumup = document.createElement('p');
+        newSumup.innerHTML = showSummary;
+        sumUpBox.appendChild(newSumup);
+        // rating
+        if (show.rating.average !== null) {
+            const showRating = show.rating.average;
+            const newRating = document.createElement('h3');
+            newRating.append(`Rating: ${showRating}`);
+            sumUpBox.appendChild(newRating);
+        }
     } catch (e) {
         const newP = document.createElement('p');
-        newP.append(`Sorry, no Tv Shows found for: ${searchedText}.`);
-        imgBox.appendChild(newP);
+        newP.append(`No information found for ${searchedText}`);
+        sumUpBox.appendChild(newP);
         console.log(e); 
     }
     
@@ -38,23 +78,12 @@ const getSingleShow = async function (searchedText) {
 
 form.addEventListener('submit', async e => {
     e.preventDefault();
-    if (imgBox.innerHTML || SumupBox.innerHTML) {
+    if (imgBox.innerHTML || sumUpBox.innerHTML) {
         imgBox.innerHTML = '';
-        SumupBox.innerHTML = '';
+        sumUpBox.innerHTML = '';
     }
     const searchedTitle = input.value;
     await getSingleShow(searchedTitle);
 })
-
-// can only make 100 requests per day
-const getImdb = async function (title) {
-        const getId = await axios.get(`https://imdb-api.com/en/API/SearchTitle/k_4d794ka6/${title}`);
-        const titleId = getId.data.results[0].id;
-        const getRating = await axios.get(`https://imdb-api.com/en/API/Ratings/k_4d794ka6/${titleId}`);
-        const ImdbRating = getRating.data.imDb;
-        const newH3 = document.createElement('h3');
-        newH3.append(`Imdb: ${ImdbRating}`);
-        imgBox.appendChild(newH3);
-}
 
 
