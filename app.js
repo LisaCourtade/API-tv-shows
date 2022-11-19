90d78887f52d8bf34f3009f0d1ca5cd1213a2933
Lisa Courtade <lisa.courtade@hotmail.fr>
2022-11-19 18:49:13 +0200

    added stylesheet and restyled list. added removal of list when new search result are added

--- app.js
+++ app.js
@@ -7,13 +7,23 @@ const getShows = async function (searchedText) {
     const result = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchedText}`);
     const shows = result.data;
     for (let el of shows) {
-        const newLi = document.createElement('li');
-        newLi.append(el.show.name);
-        list.appendChild(newLi);
         if (el.show.image) {
+            const newLi = document.createElement('li');
             const newImg = document.createElement('img');
             newImg.src = el.show.image.medium;
-            list.appendChild(newImg);
+            newImg.classList.add('li-image');
+            newLi.appendChild(newImg);
+            list.appendChild(newLi);
+            newLi.classList.add('list-item');
+        } else {
+            const newLi = document.createElement('li');
+            newLi.append(el.show.name);
+            list.appendChild(newLi);
+            const defaultImg = document.createElement('img');
+            defaultImg.src = "/slashed-image.jpg";
+            defaultImg.classList.add('li-image')
+            list.appendChild(defaultImg);
+            newLi.classList.add('list-item');
         }
     }
     
@@ -21,6 +31,9 @@ const getShows = async function (searchedText) {
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
+    if (list.innerHTML) {
+        list.innerHTML = '';
+    }
     const searchedTitle = input.value;
     await getShows(searchedTitle);
 })
